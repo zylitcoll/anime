978da3058c4b3690516e1af7dd27ad65d8f6c445
zylitcoll <student.ico.org@gmail.com>
2026-02-27 11:29:37 +0700

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel"; //
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: "server",
+  output: "server", // Mengaktifkan mode SSR
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
